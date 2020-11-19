186c16d2a492e50238dc71e10edc3a418031b661
Julia Thea <yuliya.boyadjan@gmail.com>
2020-11-18 20:44:14 -0600

    Added form POST route

--- src/app.js
+++ src/app.js
@@ -11,6 +11,39 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '../index.html'));
 });
 
+// POST route from contact form
+app.post('/', (req, res) => {
+
+    // Instantiate the SMTP server
+    const smtpTrans = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
+      auth: {
+        user: GMAIL_USER,
+        pass: GMAIL_PASS
+      }
+    })
+  
+    // Specify what the email will look like
+    const mailOpts = {
+      from: 'Your sender info here', // This is ignored by Gmail
+      to: GMAIL_USER,
+      subject: 'New message from contact form at juliathea.com',
+      text: `${req.body.name} (${req.body.email}) says: ${req.body.message}`
+    }
+  
+    // Attempt to send the email
+    smtpTrans.sendMail(mailOpts, (error, response) => {
+      if (error) {
+        res.render('contact-failure') // Show a page indicating failure
+      }
+      else {
+        res.render('contact-success') // Show a page indicating success
+      }
+    })
+  })
+
 app.listen(process.env.PORT || 8000, function () {
     console.log('Server is up on port 8000');
 });
