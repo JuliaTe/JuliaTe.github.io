d65a6e03b937de7a5c7513bdf009c8bbc0fc0392
Julia Thea <yuliya.boyadjan@gmail.com>
2021-02-27 18:10:49 -0600

    some additional configs

--- src/app.js
+++ src/app.js
@@ -2,8 +2,10 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, '../index.html'));
 });
 
 app.listen(process.env.PORT || 8000, function () {
